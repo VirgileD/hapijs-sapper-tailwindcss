1caaf3a780a88d556eba1fe8d32cdc7cf2b1874b
Virgile Devaux (a134883) <virgile.devaux@worldline.com>
2021-02-12 11:07:07 +0100

    Hapijs backend with simple authentication

---
+++ demo-plan-check-backend/routes/home.js
@@ -0,0 +1,58 @@
+'use strict';
+var JWT         = require('jsonwebtoken');   // used to sign our content
+var JWT_SECRET        = process.env.JWTKEY || 'NeverShareYourSecret';
+var aguid       = require('aguid');
+
+module.exports = [{
+    method: 'GET',
+    path: '/',
+    config: { auth: false },
+    handler: (request, h) => 'Hello'
+},
+{
+    method: 'GET',
+    path: '/prot',
+    config: { auth: 'jwt' },
+    handler: (request, h) => 'Hello prot'
+},
+{
+    method: 'GET',
+    path: '/login',
+    config: { auth: false },
+    handler: function(request, h) {
+        var session = {
+            valid: true, // this will be set to false when the person logs out
+            id: aguid(), // a random session id
+            exp: new Date().getTime() + 60 * 60 * 1000 // expires in 60 minutes time
+        };
+        // sign the session as a JWT
+        var token = JWT.sign(session, JWT_SECRET); // synchronous
+        console.log("New token created: "+token);
+        request.server.app.sessions[session.id] = session;
+        console.log("session added with id "+session.id);
+        return h.response({ message: 'logged in'})
+            .code(200)
+            .type('application/json')
+            .header("Authorization", token)
+            .state("token", token, request.server.app.cookie_options);
+    }
+},
+{
+      method: ['GET','POST'], path: "/logout", config: { auth: 'jwt' },
+      handler: function(request, h) {
+        var decoded;
+        if("Authorization" in request.headers) {
+            console.log("found token in headers");
+            decoded = JWT.decode(request.headers.authorization, JWT_SECRET);
+        } else if("token" in request.state) {
+            console.log("found token in cookies");
+            decoded = JWT.decode(request.state.token, JWT_SECRET);
+        }
+        if(decoded && "id" in decoded) {
+            delete request.server.app.sessions[decoded.id];
+        }
+        return h.response({ message: 'logged out'})
+            .code(200)
+            .type('application/json');
+      }
+}]
---
+++ demo-plan-check-backend/server.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const Hapi = require('@hapi/hapi');
+const Path = require('path');
+
+const cookie_options = {
+  ttl: 60 * 60 * 1000, // expires an hour from today
+  encoding: 'none',    // we already used JWT to encode
+  isSecure: true,      // warm & fuzzy feelings
+  isHttpOnly: true,    // prevent client alteration
+  clearInvalid: false, // remove invalid cookies
+  strictHeader: true   // don't allow violations of RFC 6265
+};
+
+const validate = async function (decoded, request, h) {
+    console.log('validating decoded'+JSON.stringify(decoded));
+    console.log('look for session in '+JSON.stringify(request.server.app.sessions));
+    
+    if(decoded.id in request.server.app.sessions &&
+        request.server.app.sessions[decoded.id].exp > new Date().getTime()) {
+      console.log('valid');
+      return { isValid: true };
+    } else {
+      console.log('not valid');
+      return { isValid: false };
+    }
+};
+
+
+const init = async () => {
+
+    const server = Hapi.server({
+        port: 3000,
+        host: 'localhost'
+    });
+    await server.register(require('hapi-auth-jwt2'));
+    server.auth.strategy('jwt', 'jwt', { key: 'NeverShareYourSecret', // Never Share your secret key
+        validate,  // validate function defined above
+        verifyOptions: {
+            algorithms: [ 'HS256' ]    // specify your secure algorithm
+        }
+    });
+    server.auth.default('jwt');
+    await server.register({
+      plugin: require('hapi-auto-route'),
+      options: {
+        routes_dir: Path.join(__dirname, 'routes')
+      }
+    });
+    server.app.sessions = {};
+    server.app.cookie_options = cookie_options;
+    await server.start();
+    console.log('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+    console.log(err);
+    process.exit(1);
+});
+
+init();
