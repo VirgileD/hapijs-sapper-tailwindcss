25344fbf64899e894cd790a9f1544d8c124b113b
Virgile Devaux (a134883) <virgile.devaux@worldline.com>
2021-02-14 23:05:09 +0100

    Login (just hit to enter), two routes (Demos+Services),  Demos'card

---
+++ demo-plan-check-frontend/src/routes/demos.svelte
@@ -0,0 +1,61 @@
+<!-- Demos -->
+<script>
+    import successkid from 'images/successkid.jpg';
+</script>
+
+<style>
+</style>
+
+<svelte:head>
+	<title>Demos</title>
+</svelte:head>
+
+<div class="rounded bg-white shadow max-w-md mx-auto">
+    <div class="rounded-lg border-2 border-wlblue shadow max-w-md mx-auto">
+        <header class="flex flex-row space-x-6">
+            <div class="my-auto">
+                <img alt="wl logo" class="mx-auto ml-2 mt-2" src="wl-small-logo-round.png" />
+            </div>
+            <div class="container my-auto">
+                <h3 class="text-lg font-bold">Demo Label</h3>
+                <div class="container">
+                    <i class="text-xs text-gray-700 fas fa-user align-middle"></i>&nbsp;
+                    <span class="text-xs text-gray-700">Virgile Devaux</span>
+                </div>
+                <div class="container">
+                    <i class="text-xs text-gray-700 fas fa-clock align-middle"></i>&nbsp;
+                    <span class="text-xs text-gray-700">2021/05/03 16:40</span>
+                </div>
+            </div>
+            <div class="container my-auto text-middle">
+                <i class="text-4xl text-wlgreen fas fa-heartbeat"></i>
+            </div>
+        </header>
+        <section class="ml-6 pt-6">
+            <div class="container my-auto text-left">
+                <i class="text-wlblue fas fa-handshake align-middle"></i>&nbsp;<span class="text-wlblue">Client Name</span>
+            </div>
+        </section>
+        <section class="pt-6">
+            <div class="flex flex-row">
+                <div class="container my-auto text-left">
+                    <i class="text-wlblue fas fa-link align-middle"></i>&nbsp;<span class="ml-2 text-wlblue">Demoed Service 1</span>
+                </div>
+                <div class="container my-auto text-middle">
+                    <span  class="text-wlgreen fas fa-heartbeat"></span>
+                </div>
+            </div>
+            <div class="flex flex-row">
+                <div class="container my-auto text-left">
+                    <i class="text-wlblue fas fa-link align-middle"></i>&nbsp;<span class="ml-2 text-wlblue">Demoed Service 1</span>
+                </div>
+                <div class="container my-auto text-middle">
+                    <span  class="text-wlgreen fas fa-heartbeat"></span>
+                </div>
+            </div>
+        </section>
+         <footer class="p-4">
+          <a href="/" class="uppercase font-bold text-sm text-blue-700 hover:underline">Details</a>        
+        </footer>
+    </div>
+</div>
---
+++ demo-plan-check-frontend/src/routes/services.svelte
@@ -0,0 +1,22 @@
+<script>
+</script>
+
+<style>
+</style>
+
+<svelte:head>
+	<title>Playing with grid</title>
+</svelte:head>
+
+<div class="w-full container text-wlgray-500 antialiased bg-white">
+  <div class="grid grid-cols-6 gap-4">
+    <div class="bg-stripes bg-stripes-wlblue bg-wlblue bg-opacity-75 h-12 rounded-md flex items-center justify-center  text-2xl font-extrabold"></div>
+    <div class="col-start-2 col-span-4 bg-wlpink h-12 rounded-md flex items-center justify-center  text-2xl font-extrabold">1</div>
+    <div class="bg-stripes bg-stripes-wlblue bg-wlblue bg-opacity-75 h-12 rounded-md flex items-center justify-center  text-2xl font-extrabold"></div>
+    <div class="col-start-1 col-end-3 bg-wlblue h-12 rounded-md flex items-center justify-center  text-2xl font-extrabold">2</div>
+    <div class="bg-stripes bg-stripes-wlblue bg-wlblue bg-opacity-75 h-12 rounded-md flex items-center justify-center  text-2xl font-extrabold"></div>
+    <div class="bg-stripes bg-stripes-wlblue bg-wlblue bg-opacity-75 h-12 rounded-md flex items-center justify-center  text-2xl font-extrabold"></div>
+    <div class="col-end-7 col-span-2 bg-wlblue h-12 rounded-md flex items-center justify-center  text-2xl font-extrabold">3</div>
+    <div class="col-start-1 col-end-7 bg-wlblue h-12 rounded-md flex items-center justify-center  text-2xl font-extrabold">4</div>
+  </div>
+</div>
